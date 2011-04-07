1b5ecb29c5c4aec96fc9f9e18e2f2ff6d101042e
Robert Keizer <robert@keizer.ca>
2011-04-07 13:52:38 -0500

    Working on using getters..

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,7 @@
 var sys		= require('sys');
 var linux	= require('../');
 console.log( "Starting tests.." );
-
+/*
 console.log( "mounts gives: " );
 console.log( sys.inspect( linux.mounts( ) ) );
 
@@ -22,6 +22,7 @@ if( linux.acpi.batteries() ){
 		console.log( linux.acpi.battery( batteryArray[x]['name'] ) );
 	}
 }
+*/
 
 console.log( "sysctl() gives:" );
 console.log( sys.inspect( linux.sysctl() ) );
