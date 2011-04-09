a32226c1ba19a1b20d20e8ea0987fa47af982f25
Robert Keizer <robert@keizer.ca>
2011-04-08 22:18:53 -0500

    Still playing with getter.

--- tests/test.js
+++ tests/test.js
@@ -1,32 +1,9 @@
-var sys		= require('sys');
-var linux	= require('../');
-console.log( "Starting tests.." );
-/*
-console.log( "mounts gives: " );
-console.log( sys.inspect( linux.mounts( ) ) );
+var	linux	= require( '../' );
+var	sys	= require( 'sys' );
+console.log( "Starting up tests.." );
 
-console.log( "ps gives: " );
-console.log( sys.inspect( linux.ps( ) ) );
-
-console.log( "ps.pid('1') gives: " );
-console.log( sys.inspect( linux.ps.pid( 1 ) ) );
-
-if( !linux.ps.pid( 9999 ) ){
-	console.log( "ps.pid('9999') returned false." );
-}
-
-if( linux.acpi.batteries() ){
-	batteryArray	= linux.acpi.batteries();
-	for( var x=0; x<batteryArray.length; x++ ){
-		console.log( "linux.acpi.battery( '" + batteryArray[x]['name'] + "' ) gives: " );
-		console.log( linux.acpi.battery( batteryArray[x]['name'] ) );
-	}
-}
-
-console.log( "sys.inspect of linux.sysctl() is: " );
+console.log( "linux.sysctl() gives" );
 console.log( sys.inspect( linux.sysctl() ) );
 
-*/
-
-console.log( "linux.sysctl().net.ipvfour.conf.all.forwarding is: ");
+console.log( "linux.sysctl().net.ipvfour.conf.all.forwarding gives" );
 console.log( linux.sysctl().net.ipvfour.conf.all.forwarding );
