065715ea8120220464ae279fa565a3f280230f2d
Robert Keizer <robert@keizer.ca>
2011-04-12 23:53:26 -0500

    Much cleaner sysctl looping. Updated README.

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,7 @@
 var	linux	= require( '../' );
 var	sys	= require( 'sys' );
+var	util	= require( 'util' );
+
 console.log( "Starting up tests.." );
 
 console.log( "linux.mounts() gives: " );
@@ -11,8 +13,8 @@ console.log( sys.inspect( linux.ps() ) );
 console.log( "linux.ps.pid(1) gives: " );
 console.log( sys.inspect( linux.ps.pid(1) ) );
 
-console.log( "linux.sysctl() gives: " );
-console.log( sys.inspect( linux.sysctl() ) );
-
 console.log( "linux.acpi.batteries() gives: " );
 console.log( sys.inspect( linux.acpi.batteries() ) );
+
+console.log( "linux.sysctl() gives: " );
+console.log( util.inspect( linux.sysctl(), true, null ) );
