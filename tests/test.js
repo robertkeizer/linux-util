5a3fe34e6f5112bc8f7ae6fca75d19fa5ff8e9e2
Robert Keizer <robert@keizer.ca>
2011-10-17 06:39:16 -0500

    Working on 0.0.3.

--- tests/test.js
+++ tests/test.js
@@ -1,23 +1,11 @@
-var	linux	= require( '../' );
-var	sys	= require( 'sys' );
+var	linux	= require( 'linux-util' );
 var	util	= require( 'util' );
 
-console.log( "Starting up tests.." );
+function out( msg ){
+	console.log( msg );
+}
 
-console.log( "linux.mountSync() gives: " );
-console.log( sys.inspect( linux.mountSync() ) );
-
-console.log( "linux.mountSync( '/dev/loop0', '/mnt/tmp', 'ext2' )" );
-console.log( sys.inspect( linux.mountSync( '/dev/loop0', '/mnt/tmp', 'ext2' ) ) );
-
-console.log( "linux.ps() gives: " );
-console.log( sys.inspect( linux.ps() ) );
-
-console.log( "linux.ps.pid(1) gives: " );
-console.log( sys.inspect( linux.ps.pid(1) ) );
-
-console.log( "linux.acpi.batteries() gives: " );
-console.log( sys.inspect( linux.acpi.batteries() ) );
-
-console.log( "linux.sysctl() gives: " );
-console.log( util.inspect( linux.sysctl(), true, null ) );
+out( util.inspect( linux.ps() ) );
+out( util.inspect( linux.ps( process.pid ) ) );
+out( util.inspect( linux.sysctl() ) );
+out( util.inspect( linux.sysctl( 'net.ipv4.conf.all.forwarding' ) ) );
