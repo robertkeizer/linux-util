62374202cc14575fbefd56aca04d37c42f4129c3
Robert Keizer <robert@keizer.ca>
2011-10-18 01:04:32 -0500

    Added spot for making pretty mac address in js rather than in C.. Updated readme and package.json.

--- tests/test.js
+++ tests/test.js
@@ -9,3 +9,7 @@ out( util.inspect( linux.ps() ) );
 out( util.inspect( linux.ps( process.pid ) ) );
 out( util.inspect( linux.sysctl() ) );
 out( util.inspect( linux.sysctl( 'net.ipv4.conf.all.forwarding' ) ) );
+
+out( util.inspect( linux.interfaces.list() ) );
+out( util.inspect( linux.interfaces.get_details( "lo" ) ) );
+out( util.inspect( linux.interfaces.get_details( "wlan0" ) ) );
