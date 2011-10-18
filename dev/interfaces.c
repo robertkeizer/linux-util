f950dff26f5ae20053a44bbd6cb062d27a36f14f
Robert Keizer <robert@keizer.ca>
2011-10-17 20:42:38 -0500

    Added some C/C++ development stuffs.

---
+++ dev/interfaces.c
@@ -0,0 +1,36 @@
+#include <sys/ioctl.h>
+#include <net/if.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+
+int main( ){
+	char	buf[1024];
+	struct	ifconf	ifc;
+	struct	ifreq 	*ifr;
+	int	sck, n_interfaces, i;
+
+	sck	= socket( AF_INET, SOCK_DGRAM, 0 );
+	if( sck < 0 ){
+		perror( "Couldn't open a socket." );
+		return 1;
+	};
+
+	ifc.ifc_len	= sizeof( buf );
+	ifc.ifc_buf	= buf;
+	if( ioctl( sck, SIOCGIFCONF, &ifc ) < 0 ){
+		perror( "Coulndn't get list of interfaces.." );
+		return 1;
+	}
+
+	ifr		= ifc.ifc_req;
+	n_interfaces	= ifc.ifc_len / sizeof( struct ifreq );
+	for( i=0; i<n_interfaces; i++ ){
+		struct ifreq *item = &ifr[i];
+		printf( "%s:%s\n",
+			item->ifr_name,								/* Interface name */
+			inet_ntoa( ( (struct sockaddr_in *)&item->ifr_addr)->sin_addr ) 	/* ipv4 address. */
+		);
+	};
+
+	return 0;
+}
